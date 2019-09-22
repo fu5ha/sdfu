c1b953e517008018eb4a511a2cd298ac9d78883a
Gray Olson <gray@grayolson.com>
2019-09-22 00:20:09 -0700

    add doc comments for estimators

--- src/util.rs
+++ src/util.rs
@@ -7,6 +7,15 @@ pub trait NormalEstimator<T, V> {
     fn estimate_normal<S: SDF<T, V>>(&self, sdf: S, p: V) -> V;
 }
 
+/// Estimates the normal of an SDF by estimating the gradient of the SDF.
+/// The gradient is estimated by taking two samples of the SDF in each dimension,
+/// one slightly above (by `eps` distance) the point in question and one slightly below it and taking their
+/// difference, hence the 'central difference'. This estimation is relatively robust and accurate,
+/// but is also relatively slow since it takes 6 samples of the SDF. See the `TetrahedralEstimator` for
+/// an estimator which is slightly less robust/accurate but also slightly faster.
+/// 
+/// See [this article](http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm)
+/// for more.
 pub struct CentralDifferenceEstimator<T, V, D> {
     pub eps: T,
     _pdv: std::marker::PhantomData<V>,
@@ -15,6 +24,7 @@ pub struct CentralDifferenceEstimator<T, V, D> {
 
 impl<T, V: Vec<T>> CentralDifferenceEstimator<T, V, <V as Vec<T>>::Dimension>
 {
+    /// Creates a `CentralDifferenceEstimator` with a given epsilon value.
     pub fn new(eps: T) -> Self { CentralDifferenceEstimator { eps, _pdv: std::marker::PhantomData, _pdd: std::marker::PhantomData } }
 }
 
@@ -79,6 +89,7 @@ impl<V: Vec<f64>> Default for CentralDifferenceEstimator<f64, V, <V as Vec<f64>>
     }
 }
 
+/// Estimates the normal of an `sdf` using an `estimator`, by default a CentralDifferenceEstimator.
 pub struct EstimateNormal<T, V: Vec<T>, S, E=CentralDifferenceEstimator<T, V, <V as Vec<T>>::Dimension>> {
     pub sdf: S,
     pub estimator: E,
@@ -91,6 +102,8 @@ where E: NormalEstimator<T, V> + Default,
     S: SDF<T, V>,
     V: Vec<T>
 {
+    /// Creates a new `EstimateNormal` with an SDF using the default version of the
+    /// estimator type.
     pub fn new(sdf: S) -> Self {
         EstimateNormal {
             sdf,
@@ -106,6 +119,17 @@ where E: NormalEstimator<T, V>,
     S: SDF<T, V>,
     V: Vec<T>,
 {
+    /// Creates a new `EstimateNormal` with an SDF and a provided estimator.
+    pub fn new_with_estimator(sdf: S, estimator: E) -> Self {
+        EstimateNormal {
+            sdf,
+            estimator,
+            _pdt: std::marker::PhantomData,
+            _pdv: std::marker::PhantomData,
+        }
+    }
+
+    /// Estimates the normal of the owned SDF at point p.
     pub fn normal_at(&self, p: V) -> V {
         self.estimator.estimate_normal(self.sdf, p)
     }
